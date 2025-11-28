f8ce14da7b77418c0811d9af624990db6236fd37
dutabrahmana <dutalinggau25@gmail.com>
2025-11-28 09:58:03 +0700

    membenarkan typo

--- app/src/main/java/com/example/roomdatabase/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/roomdatabase/room/DatabaseSiswa.kt
@@ -1,8 +1,13 @@
 package com.example.roomdatabase.room
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
 @Database(entities = [Siswa::class], version =  1, exportSchema = false)
 abstract class DatabaseSiswa : RoomDatabase(){
-    abstract fun siswaDao() : SiswaDAO
+    abstract fun siswaDao() : SiswaDao
 
     companion object {
         @Volatile
