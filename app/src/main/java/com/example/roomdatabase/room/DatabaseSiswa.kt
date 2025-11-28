430b00d69caeec8155cfd6320f0d8c7f4bc8314e
dutabrahmana <dutalinggau25@gmail.com>
2025-11-28 09:53:30 +0700

    Implementasi awal Room Database untuk entitas Siswa, mencakup definisi kelas DatabaseSiswa yang merupakan singleton dan mengintegrasikan SiswaDAO sebagai Data Access Object untuk operasi database.

--- app/src/main/java/com/example/roomdatabase/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/roomdatabase/room/DatabaseSiswa.kt
@@ -1,4 +1,10 @@
 package com.example.roomdatabase.room
 
-class DatabaseSiswa {
-}
+@Database(entities = [Siswa::class], version =  1, exportSchema = false)
+abstract class DatabaseSiswa : RoomDatabase(){
+    abstract fun siswaDao() : SiswaDAO
+
+    companion object {
+        @Volatile
+        private var instance: DatabaseSiswa? = null
+
