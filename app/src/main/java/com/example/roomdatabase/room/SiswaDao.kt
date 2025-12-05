46c9ea00242f9fab6abe071dcab13a376e93f9f8
dutabrahmana <dutalinggau25@gmail.com>
2025-12-05 10:32:55 +0700

    memperbaiki error

--- app/src/main/java/com/example/roomdatabase/room/SiswaDao.kt
+++ app/src/main/java/com/example/roomdatabase/room/SiswaDao.kt
@@ -8,7 +8,6 @@ import androidx.room.Query
 import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
-
 @Dao
 interface SiswaDao {
     @Query(value = "SELECT * from tblSiswa ORDER BY nama ASC")
@@ -17,13 +16,13 @@ interface SiswaDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(siswa : Siswa)
 
-    @Query(value = "SELECT *from tblSiswa WHERE id = id")
+    @Query(value = "SELECT * from tblSiswa WHERE id = :id")
     fun getSiswa(id: Int): Flow<Siswa>
 
     @Delete
     suspend fun delete(siswa: Siswa)
 
     @Update
-    suspend fun  update(siswa: Siswa)
+    suspend fun update(siswa: Siswa)
 
 }
